293eef2452ea1985beac7b713cebb8dab6c84dda
justine-135 <ujusine@yahoo.com>
2023-03-28 16:45:47 +0800

    updated

--- includes/student_view.inc.php
+++ includes/student_view.inc.php
@@ -4,11 +4,12 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/sabanges/classes/Views/student.class.ph
 use Views\StudentView;
 use Views\StudentInformationView;
 
-$id;
+$id = null;
 
 isset($_POST['id']) ? $id = $_POST['id'] : "";
 isset($_POST['informations']) ? header('Location: ../student_informations.php') : '';
 
 $view === "masterlist" ? $obj = new StudentView() : (($view === "student_information") ? $obj = new StudentInformationView() : "");
 $view === "masterlist" ? $obj->initIndex() : (($view === "student_information") ? $obj->initSingleIndex($id) : "");
+
 
