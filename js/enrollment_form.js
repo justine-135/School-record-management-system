acc5cfa5662efae0b8834370eb99fc7784ff1be3
Justine <ujusine@yahoo.com>
2023-03-29 16:52:12 +0800

    disabled form validation

--- js/enrollment_form.js
+++ js/enrollment_form.js
@@ -22,13 +22,13 @@ educ_radios.forEach((element) => {
 });
 
 // Fill 'School year' textbox with current year
-const from_sy_textbox = document.querySelector(".from-sy-textbox");
-const to_sy_textbox = document.querySelector(".to-sy-textbox");
+// const from_sy_textbox = document.querySelector(".from-sy-textbox");
+// const to_sy_textbox = document.querySelector(".to-sy-textbox");
 
-let date = new Date();
+// let date = new Date();
 
-from_sy_textbox.min = date.getFullYear();
-from_sy_textbox.value = date.getFullYear();
+// from_sy_textbox.min = date.getFullYear();
+// from_sy_textbox.value = date.getFullYear();
 
-to_sy_textbox.min = date.getFullYear();
-to_sy_textbox.value = date.getFullYear() + 1;
+// to_sy_textbox.min = date.getFullYear();
+// to_sy_textbox.value = date.getFullYear() + 1;
