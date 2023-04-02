ff442a9c9cb2a1e8e76aa64570329a79c37e118c
Justine <ujusine@yahoo.com>
2023-04-02 19:36:45 +0800

    updated

--- masterlist.php
+++ masterlist.php
@@ -5,26 +5,6 @@
 
 <?php include "partials/nav.php"; ?>
 
-<main class="container-fluid w-90 border mt-4 p-4 bg-white">
-  <h4 class="">Masterlist</h4>
-  <div class="border mt-3">
-    <div>
-      <div class="d-flex justify-content-between align-items-center pt-3 px-3 border-bottom">
-        <h5>Enrolled</h5>
-        <form class="input-group mb-3 w-25" action="">
-            <input type="text" class="form-control" placeholder="Enter here" />
-            <button
-            class="btn btn-primary"
-            id="button-addon2"
-            type="submit"
-            >
-            Search
-            </button>
-        </form> 
-      </div>
-    </div>
-    <div class="table-responsive">
-      <?php include 'includes/student_view.inc.php'; ?>
-    </div>
-  </div>
-</main>
+<main class="container-fluid w-90 border mt-4 p-4 bg-white masterlist"></main>
+
+<script src="js/masterlist.js"></script>
