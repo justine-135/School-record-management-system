878b638b3b39bc445e0abf124a14c1c560e49e33
Justine <ujusine@yahoo.com>
2023-04-02 19:36:55 +0800

    updated

--- student_informations.php
+++ student_informations.php
@@ -1,10 +1,13 @@
 <?php include "partials/header.php"; ?>
 
-<?php $header = "/student_information"; ?>
+<?php $header = "/student_informations"; ?>
 
-<?php $view="student_information"; ?>
+<?php $view="student_informations"; ?>
 
 <?php include "partials/nav.php"; ?>
 
-<?php include "includes/student_view.inc.php"; ?>
-<script src="js/student_profile.js"></script>
+<main class="container-fluid w-90 border mt-4 p-4 bg-white student-information">
+<?php include 'includes/student.inc.php'; ?>
+</main>
+
+<script src="js/student_informations.js"></script>
