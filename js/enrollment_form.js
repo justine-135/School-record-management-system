daa96dfef3a87ad32b755ff52b52e8a16d8dca3b
Justine <ujusine@yahoo.com>
2023-04-04 16:36:18 +0800

    updated

--- js/enrollment_form.js
+++ js/enrollment_form.js
@@ -32,16 +32,22 @@ from_sy_textbox.min = date.getFullYear();
 to_sy_textbox.min = date.getFullYear();
 // to_sy_textbox.value = date.getFullYear() + 1;
 
+const gradeSelect = document.querySelector(".grade-select");
 const loadGradeSelect = () => {
+  let gradeLvl;
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4) {
       // Show HTML
-      const gradeSelect = document.querySelector(".grade-select");
       gradeSelect.innerHTML = this.responseText;
     }
   };
-  xmlhttp.open("GET", "./includes/operations.inc.php?grade_level_select", true);
+  gradeLvl = gradeSelect.previousElementSibling.value;
+  xmlhttp.open(
+    "GET",
+    "./includes/operations.inc.php?grade_level_select=" + gradeLvl,
+    true
+  );
   xmlhttp.send();
 };
 
