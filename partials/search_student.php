10c0d010dc94d08cd3c11c9ae6b6b96c78050f43
Justine <ujusine@yahoo.com>
2023-04-06 20:58:56 +0800

    added placeholders to text

--- partials/search_student.php
+++ partials/search_student.php
@@ -1,11 +1,11 @@
-<form class="input-group mb-3 w-25 search-form" method="get" enctype="multipart/form-data">
-    <input type="text" class="form-control search-input" placeholder="Enter here" name="query" />
+<form class="input-group w-25 search-form" method="get" enctype="multipart/form-data">
+    <input type="text" class="form-control search-input" placeholder="Search" name="query" />
     <input type="hidden" class="form-control" placeholder="Enter here" name="view" value="masterlist" />
     <button
     class="btn btn-primary search-btn"
     id="button-addon2"
     type="submit"
     >
-    Search
+    Filter
     </button>
 </form> 
