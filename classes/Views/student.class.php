546af915dfa39db23664455ae5c7f8cf27c82f1b
Justine <ujusine@yahoo.com>
2023-04-06 21:00:52 +0800

    added grades section

--- classes/Views/student.class.php
+++ classes/Views/student.class.php
@@ -7,10 +7,17 @@ class StudentView extends \Models\Student{
     public function initIndex($query){
         $results = $this->index($query);
         ?>
-        <table class="table table-hover mb-0 student-table">
+        <table class="table table-hover mb-0 border-top student-table">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
+                    <th scope="col">     
+                        <div class="d-flex">
+                            <span class="me-2">#</span>                  
+                            <div class="form-check">
+                                <input class="form-check-input masterlist-chkbox-all" type="checkbox" value="" id="flexCheckDefault">
+                            </div>
+                        </div>
+                    </th>
                     <th scope="col">LRN</th>
                     <th scope="col">Student</th>
                     <th scope="col">Enrolled at</th>
@@ -24,32 +31,41 @@ class StudentView extends \Models\Student{
                     foreach ($results as $row) {
                 ?>
                     <tr>
-                        <th scope="row"><?= $row['id'] ?></th>
+                        <td>
+                            <div class="d-flex">
+                                <span class="me-2">
+                                <?= $row['id'] ?>
+                                </span>
+                                <div class="form-check">
+                                    <input class="form-check-input masterlist-chkbox" type="checkbox" value="<?= $row['id'] ?>" id="flexCheckDefault">
+                                </div>
+                            </div>
+                        </td>
                         <td><?= $row['lrn'] ?></td>
                         <td><?= strtoupper($row['surname']) . ', ' . strtoupper($row['first_name']) . ' ' . strtoupper($row['middle_name'])  ?></td>
                         <td><?= $row['enrolled_at'] ?></td>
                         <td><?= $row['gender'] ?></td>
                         <td><?= $row['grade_level'] ?></td>
                         <td>
-                        <div class="dropdown ml-auto">
-                            <a
-                            class="btn dropdown-toggle btn-primary"
-                            href="#"
-                            role="button"
-                            id="dropdownMenuLink"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                            >
-                            View
-                            </a>
-                            
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <input class="student-id" type="hidden" name="id" id="id" value="<?= $row['id'] ?>" >
-                                <!-- <li><input type="submit" class="dropdown-item information-links" name="information" value="Information"></li> -->
-                                <li><a class="dropdown-item" href="../sabanges/student_informations.php?id=<?= $row['id']?>">Informations</a></li>
-                                <li><a class="dropdown-item" href="#">Grades</a></li>
-                            </ul>
-                        </div>
+                            <div class="dropdown ml-auto">
+                                <a
+                                class="btn dropdown-toggle btn-primary"
+                                href="#"
+                                role="button"
+                                id="dropdownMenuLink"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                                >
+                                View
+                                </a>
+                                
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                    <input class="student-id" type="hidden" name="id" id="id" value="<?= $row['id'] ?>" >
+                                    <!-- <li><input type="submit" class="dropdown-item information-links" name="information" value="Information"></li> -->
+                                    <li><a class="dropdown-item" href="../sabanges/student_informations.php?id=<?= $row['id']?>">Informations</a></li>
+                                    <li><a class="dropdown-item" href="../sabanges/student_informations.php?id=<?= $row['id']?>#grades-section">Grades</a></li>
+                                </ul>
+                            </div>
                         </td>
                     </tr>
                 <?php 
@@ -67,11 +83,14 @@ class StudentView extends \Models\Student{
 class StudentInformationView extends \Models\Student{
     public function initSingleIndex($id){
         $result = $this->singleIndex($id);
+        foreach ($result as $row) {
+            $result2 = $this->subjectIndex($row['grade']);
+        }
         // count($result) === 0 ? header("Location: ./student_informations.php?id={$id}&err=not_found") : "";
         ?>
         <h4 class="">Profile</h4>
-        <div class="row">
-            <div class="border mt-3 col-md-6">
+        <div class="row gap-3">
+            <div class="border mt-3 col-md">
                 <div class="row ">
                     <div class="d-flex align-items-center justify-content-between py-3 px-3 border-bottom">
                         <h5>Information</h5>
@@ -86,14 +105,18 @@ class StudentInformationView extends \Models\Student{
                         </div>
                         <div class="ms-3 py-1 w-75">
                         <?php foreach($result as $row){ ?>
+                            <?php if ($row['status'] == "Active") {
+                            ?>
                             <h3 class="text-end"><?= strtoupper($row['surname']) . ", " . strtoupper($row['first_name']) . " " . strtoupper($row['middle_name']) ?></h3>
                             <span class="d-block text-end"> <?= strtoupper($row['lrn']) ?></span>
                             <span class="d-block text-end">Grade level - <?= $row['grade_level'] ?></span>
-                        <?php } ?>
+                        <?php }} ?>
                         </div>
                     </div>
                     <div class="">
                         <?php foreach($result as $row){ ?>
+                            <?php if ($row['status'] == "Active") {
+                            ?>
                         <div class="w-100 text-end">
                             <div class="py-2 px-3">
                                 <div class="d-flex align-items-center justify-content-between">
@@ -166,19 +189,37 @@ class StudentInformationView extends \Models\Student{
                             </div>
                         </div>
                         
-                        <?php } ?>
+                        <?php }} ?>
                     </div>
                 </div>
             </div>
-            <div class="border mt-3 col-md-6">
-            <?php foreach($result as $row){ ?>
+            <div class="container border mt-3 col-md">
                 <div class="row">
-                    <div class="d-flex justify-content-between align-items-center py-3 px-3 border-bottom">
+                    <div class=" py-3 px-3 border-bottom">
                         <h5>Subjects</h5>
                     </div>
-                    <div class="d-flex justify-content-between align-items-center py-3 px-3 border-bottom">
+                    <div class="py-2">
+                        <div class="accordion" id="accordionPanelsStayOpenExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+                                        Accordion Item #1
+                                    </button>
+                                    </h2>
+                                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+                                        <div class="accordion-body">
+                                        <?php foreach($result2 as $row2){ ?>
+                                        <?= $row2['subject'] ?>
+                                        <?php } ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <div class=" py-3 px-3 border-bottom border-top">
                         <h5>Enrolled History</h5>
                     </div>
+                    <?php foreach($result as $row){ ?>
                     <div class="w-100 py-2 px-3 border-bottom">
                         <div class="d-flex align-items-center justify-content-between">
                             <span class="fw-bold">School year :</span>
@@ -194,12 +235,130 @@ class StudentInformationView extends \Models\Student{
                         </div>
                         <div class="d-flex align-items-center justify-content-between">
                             <span class="fw-bold">Status :</span>
+                            <?php if ($row['status'] == "Active") {
+                            ?>
                             <span class="fw-bold text-success"><?= $row['status'] ?></span>
+                            <?php } else{
+                                ?>
+                            <span class="fw-bold text-danger"><?= $row['status'] ?></span>
+                            <?php
+                            } ?>
                         </div>
                     </div>
-                    
+                    <?php } ?>
                 </div>
-            <?php } ?>
+            </div>
+        </div>
+        <?php include './partials/add_grades_modal.php'; ?>
+        <div class="row" id="grades-section">
+            <div class="border mt-3 col-md">
+                <div class="d-flex align-items-center justify-content-between py-3 px-3">
+                    <h5>Grades</h5>          
+                    <a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Open first modal</a>     
+                </div>
+                <div class="accordion" id="accordionPanelsStayOpenExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#grade-table-1" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+                            Accordion Item #1
+                        </button>
+                        </h2>
+                        <div id="grade-table-1" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                            <div class="accordion-body">
+                                <form action=""></form>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Subject</th>
+                                            <th>1st quarter</th>
+                                            <th>2nd quarter</th>
+                                            <th>3rd quarter</th>
+                                            <th>4th quarter</th>
+                                            <th>Average</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Science</td>
+                                            <td><input type="text" name="" id=""></td>
+                                            <td><input type="text" name="" id=""></td>
+                                            <td><input type="text" name="" id=""></td>
+                                            <td><input type="text" name="" id=""></td>
+                                            <td><input type="text" name="" id="" disabled></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Science</td>
+                                            <td><input type="text" name="" id=""></td>
+                                            <td><input type="text" name="" id=""></td>
+                                            <td><input type="text" name="" id=""></td>
+                                            <td><input type="text" name="" id=""></td>
+                                            <td><input type="text" name="" id="" disabled></td>
+                                        </tr>
+                                        
+                                    </tbody>
+                                </table>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#grade-table-2" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+                            Accordion Item #1
+                        </button>
+                        </h2>
+                        <div id="grade-table-2" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                            <div class="accordion-body">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Subject</th>
+                                            <th>1st quarter</th>
+                                            <th>2nd quarter</th>
+                                            <th>3rd quarter</th>
+                                            <th>4th quarter</th>
+                                            <th>Average</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Science</td>
+                                            <td>80</td>
+                                            <td>76</td>
+                                            <td>90</td>
+                                            <td>80</td>
+                                            <td>82</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Science</td>
+                                            <td>80</td>
+                                            <td>76</td>
+                                            <td>90</td>
+                                            <td>80</td>
+                                            <td>82</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Science</td>
+                                            <td>80</td>
+                                            <td>76</td>
+                                            <td>90</td>
+                                            <td>80</td>
+                                            <td>82</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Science</td>
+                                            <td>80</td>
+                                            <td>76</td>
+                                            <td>90</td>
+                                            <td>80</td>
+                                            <td>82</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <?php
