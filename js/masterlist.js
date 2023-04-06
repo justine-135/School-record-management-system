c76a1834a5d24ad93bc136cb2f6393480b54d0e1
Justine <ujusine@yahoo.com>
2023-04-06 21:00:17 +0800

    check uncheck all checkboxes

--- js/masterlist.js
+++ js/masterlist.js
@@ -5,6 +5,35 @@ const loadMasterList = (query) => {
       // Show HTML
       const masterlistTable = document.querySelector(".masterlist-table");
       masterlistTable.innerHTML = this.responseText;
+
+      // Checkbox all student
+      const masterlistChkbxAll = document.querySelector(
+        ".masterlist-chkbox-all"
+      );
+      const masterlistChkbxs = document.querySelectorAll(".masterlist-chkbox");
+
+      let checkAll = false;
+
+      // Uncheck and check checkboxes
+      const checkUncheck = (bool1) => {
+        masterlistChkbxs.forEach((element) => {
+          let chkBx = element;
+
+          chkBx.checked = bool1;
+          checkAll = bool1;
+        });
+      };
+
+      // Checkbox conditions
+      masterlistChkbxAll.addEventListener("change", () => {
+        if (!checkAll) {
+          // Check all
+          checkUncheck(true);
+        } else {
+          // Uncheck all
+          checkUncheck(false);
+        }
+      });
     }
   };
   if (query === undefined) {
