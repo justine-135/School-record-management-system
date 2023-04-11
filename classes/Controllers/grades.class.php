7b9ae0f6472788010d0c8a544f98e333fbea1001
Justine <ujusine@yahoo.com>
2023-04-11 15:34:59 +0800

    update

--- classes/Controllers/grades.class.php
+++ classes/Controllers/grades.class.php
@@ -7,11 +7,17 @@ include $_SERVER['DOCUMENT_ROOT'].'/sabanges/classes/Models/grades.class.php';
 class GradesController extends \Models\Grades{
     public function initCreate($id, $lrn, $grade_lvl, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter){
         if ($this->initGradesExists($lrn, $grade_lvl, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter) !== false) {
-            header("Location: ../student_informations.php?id=" . $id . "&grade_exist#grades-section");
+            header("Location: ../student_informations.php?id=" . $id . "&err=grade_exist#grades-section");
+            die();
+        }
+        elseif ($this->emptyInputs($lrn, $grade_lvl, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter) !== false) {
+            header("Location: ../student_informations.php?id=" . $id . "&err=empty_input#grades-section");
+            die();
         }
         else{
             $this->create($lrn, $grade_lvl, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter);
             header("Location: ../student_informations.php?id=" . $id . "&added#grades-section");
+            die();
         }
     }
 
@@ -19,6 +25,17 @@ class GradesController extends \Models\Grades{
         $result = false;
         if (count($this->gradesExists($lrn, $grade_lvl, $subjects)) > 0) {
             $result = true;
+        }
+        return $result;
+    }
+
+    protected function emptyInputs($lrn, $grade_lvl, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter){
+        $result = false;
+        for ($i=0; $i < count($subjects); $i++) { 
+            if (empty($lrn) || empty($grade_lvl) || empty($subjects[$i]) || empty($first_quarter[$i]) || empty($second_quarter[$i]) || empty($third_quarter[$i]) || empty($fourth_quarter[$i])) {
+                $result = true;
+                break;
+            }
         }
         return $result;
     }
