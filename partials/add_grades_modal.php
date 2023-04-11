266abc0069c62cf6371c6f4081a0904b47a49b95
Justine <ujusine@yahoo.com>
2023-04-11 12:31:24 +0800

    add grade form

--- partials/add_grades_modal.php
+++ partials/add_grades_modal.php
@@ -1,64 +1,31 @@
-<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-  <div class="modal-dialog modal-fullscreen modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalToggleLabel">Modal 1</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <form action="">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Subject</th>
-                    <th>1st quarter</th>
-                    <th>2nd quarter</th>
-                    <th>3rd quarter</th>
-                    <th>4th quarter</th>
-                    <th>Average</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Science</td>
-                    <td><input type="text" name="" id=""></td>
-                    <td><input type="text" name="" id=""></td>
-                    <td><input type="text" name="" id=""></td>
-                    <td><input type="text" name="" id=""></td>
-                    <td><input type="text" name="" id="" disabled></td>
-                </tr>
-                <tr>
-                    <td>Science</td>
-                    <td><input type="text" name="" id=""></td>
-                    <td><input type="text" name="" id=""></td>
-                    <td><input type="text" name="" id=""></td>
-                    <td><input type="text" name="" id=""></td>
-                    <td><input type="text" name="" id="" disabled></td>
-                </tr>
-                
-            </tbody>
-        </table>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Open second modal</button>
+<form action="./includes/grades.inc.php" method="post" enctype="multipart/form-data">
+  <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+    <div class="modal-dialog modal-fullscreen modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalToggleLabel">Modal 1</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <select class="form-select add-grade-select" name="grade-lvl" id="grade-lvl">
+            <option value="Kindergarten" selected disabled>Select grade level ---</option>
+            <option value="Kindergarten">Kindergarten</option>
+            <option value="1">Grade 1</option>
+            <option value="2">Grade 2</option>
+            <option value="3">Grade 3</option>
+            <option value="4">Grade 4</option>
+            <option value="5">Grade 5</option>
+            <option value="6">Grade 6</option>
+          </select>
+          <table class="table add-grade-table "></table>
+          <input type="text" name="lrn" value="<?= $result[0]['lrn'] ?>" id="lrn">
+          <input type="text" name="id" value="<?= $_GET['id'] ?>" id="lrn">
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-primary" type="submit" name="submit-grade" value="submit">Submit</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
-<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Hide this modal and show the first with the button below.
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" data-bs-dismiss="modal">Back to first</button>
-      </div>
-    </div>
-  </div>
-</div>
+</form>
+
