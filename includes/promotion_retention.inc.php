85f0b7f3764e6f49f81378c717fa298ab0c1e8ef
Justine <ujusine@yahoo.com>
2023-04-12 16:47:25 +0800

    get data

--- includes/promotion_retention.inc.php
+++ includes/promotion_retention.inc.php
@@ -1,5 +1,26 @@
 <?php
 
+require '../classes/Controllers/promotion_retention.class.php';
+
+use Controllers\PromotionRetentionController;
+
+function promote($ids, $lrns, $grade_levels){
+    $obj = new PromotionRetentionController();
+    $obj->initPromote($ids, $lrns, $grade_levels);
+}
+
 if (isset($_POST['promote'])) {
-    echo $_POST['promote'];
+    $chkbox_students = $_POST['chkbox-student'];
+    $ids = array();
+    $lrns = array();
+    $grade_levels = array();
+
+    foreach ($chkbox_students as $key => $obj) {
+        $data = explode(",",$obj);
+        array_push($ids, $data[0]);
+        array_push($lrns, $data[1]);
+        array_push($grade_levels, $data[2]);
+    }
+
+    promote($ids, $lrns, $grade_levels);
 }
