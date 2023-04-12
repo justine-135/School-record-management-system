0dd9a801a40f17010807e79e5f73e397864fa9c0
Justine <ujusine@yahoo.com>
2023-04-12 16:46:32 +0800

    update

--- student_informations.php
+++ student_informations.php
@@ -4,6 +4,7 @@
 <?php $view="student-informations"; ?>
 
 <?php include "partials/nav.php"; ?>
+<?php include 'partials/alert.php'; ?>
 
 <main class="container-fluid w-90 border mt-4 p-4 bg-white <?= $view ?>">
 <?php include 'includes/student.inc.php'; ?>
