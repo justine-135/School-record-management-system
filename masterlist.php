8685bd8062987ed8d2ab8f2e3519604e3fb41405
Justine <ujusine@yahoo.com>
2023-04-19 14:17:34 +0800

    update

--- masterlist.php
+++ masterlist.php
@@ -1,3 +1,9 @@
+<?php 
+// if (empty($_SESSION['username'])) {
+//   header("Location: ./login.php");
+// }
+?>
+
 <?php include "partials/header.php"; ?>
 
 <?php $header = "/masterlist"; ?>
@@ -5,6 +11,12 @@
 <?php $h4="Masterlist"; ?>
 
 <?php include "partials/nav.php"; ?>
+
+<?php
+if (empty($_SESSION['username']) && empty($_SESSION['account_id'])) {
+  header("Location: ./login.php");
+}
+?>
 
 <main class="container-fluid w-90 border mt-4 p-4 bg-white <?= $view ?>">
   <h4 class=""><?= $h4 ?></h4>
@@ -17,6 +29,8 @@
 
         <?php require $_SERVER['DOCUMENT_ROOT'].'/sabanges/partials/search_student.php'; ?>
 
+        <?php require $_SERVER['DOCUMENT_ROOT'].'/sabanges/partials/filter_active_students.php'; ?>
+
       </div>
     </div>
     <form class="" action="./includes/promotion_retention.inc.php" method="post" enctype="multipart/form-data">
@@ -24,10 +38,13 @@
         <button type="submit" value="promote" name="promote" class="btn btn-primary">Promote</button>
         <button type="submit" value="retian" name="retain" class="btn btn-danger">Retain</button>
       </div>
-      <div class="table-responsive <?= $view ?>-table"></div>
+      <div class="table-responsive <?= $view ?>-table min-vh-100"></div>
     </form>
   </div>
 
 </main>
+
+
+<?php require $_SERVER['DOCUMENT_ROOT'].'/sabanges/partials/footer.php'; ?>
 
 <script src="js/masterlist.js"></script>
