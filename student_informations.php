97a687f0ce230aa6b1f9e153e85be98ed3b1d656
Justine <ujusine@yahoo.com>
2023-04-19 14:20:16 +0800

    update

--- student_informations.php
+++ student_informations.php
@@ -6,8 +6,15 @@
 <?php include "partials/nav.php"; ?>
 <?php include 'partials/alert.php'; ?>
 
+<?php
+if (empty($_SESSION['username']) && empty($_SESSION['account_id'])) {
+  header("Location: ./login.php");
+}
+?>
+
 <main class="container-fluid w-90 border mt-4 p-4 bg-white <?= $view ?>">
 <?php include 'includes/student.inc.php'; ?>
 </main>
+<?php require $_SERVER['DOCUMENT_ROOT'].'/sabanges/partials/footer.php'; ?>
 
 <script src="js/student_informations.js"></script>
