e6db769969502110f8e629aa4a105de0fdaba8b6
Justine <ujusine@yahoo.com>
2023-04-19 14:17:23 +0800

    update

--- index.php
+++ index.php
@@ -4,7 +4,14 @@
 
 <?php include "partials/nav.php"; ?>
 
+<?php
+if (empty($_SESSION['username']) && empty($_SESSION['account_id'])) {
+  header("Location: ./login.php");
+}
+?>
+
 <main class="container-fluid w-90 border mt-4 p-4 bg-white">
   <h4>Home</h4>
   <p class="">Welcome to homepage</p>
 </main>
+<?php require $_SERVER['DOCUMENT_ROOT'].'/sabanges/partials/footer.php'; ?>
