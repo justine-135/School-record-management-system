40c309cf3b85d4ad7e121f57962e98f7004a802b
Justine <ujusine@yahoo.com>
2023-04-25 08:31:09 +0800

    removed ajax

--- js/masterlist.js
+++ js/masterlist.js
@@ -1,80 +1,94 @@
-const loadMasterList = (query, statusValue) => {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4) {
-      // Show HTML
-      const masterlistTable = document.querySelector(".masterlist-table");
-      masterlistTable.innerHTML = this.responseText;
+// const loadMasterList = () => {
+//   var xmlhttp = new XMLHttpRequest();
+//   let page = document.querySelector(".get-page").value;
+//   let rowValue = document.querySelector(".get-row").value;
+//   let statusValue = document.querySelector(".get-status").value;
 
-      // Checkbox all student
-      const masterlistChkbxAll = document.querySelector(
-        ".masterlist-chkbox-all"
-      );
-      const masterlistChkbxs = document.querySelectorAll(".masterlist-chkbox");
+//   xmlhttp.onreadystatechange = function () {
+//     if (this.readyState == 4) {
+//       // Show HTML
+//       const masterlistTable = document.querySelector(".masterlist-table");
+//       masterlistTable.innerHTML = this.responseText;
 
-      let checkAll = false;
+//       // Checkbox all student
+//       // const masterlistChkbxAll = document.querySelector(
+//       //   ".masterlist-chkbox-all"
+//       // );
+//       // const masterlistChkbxs = document.querySelectorAll(".masterlist-chkbox");
 
-      // Uncheck and check checkboxes
-      const checkUncheck = (bool1) => {
-        masterlistChkbxs.forEach((element) => {
-          let chkBx = element;
+//       // let checkAll = false;
 
-          chkBx.checked = bool1;
-          checkAll = bool1;
-        });
-      };
+//       // // Uncheck and check checkboxes
+//       // const checkUncheck = (bool1) => {
+//       //   masterlistChkbxs.forEach((element) => {
+//       //     let chkBx = element;
 
-      // Checkbox conditions
-      masterlistChkbxAll.addEventListener("change", () => {
-        if (!checkAll) {
-          // Check all
-          checkUncheck(true);
-        } else {
-          // Uncheck all
-          checkUncheck(false);
-        }
-      });
-    }
-  };
+//       //     chkBx.checked = bool1;
+//       //     checkAll = bool1;
+//       //   });
+//       // };
 
-  xmlhttp.open(
-    "GET",
-    "./includes/student.inc.php?query=" + query + "&status=" + statusValue,
-    true
-  );
-  xmlhttp.send();
-};
+//       // // Checkbox conditions
+//       // masterlistChkbxAll.addEventListener("change", () => {
+//       //   if (!checkAll) {
+//       //     // Check all
+//       //     checkUncheck(true);
+//       //   } else {
+//       //     // Uncheck all
+//       //     checkUncheck(false);
+//       //   }
+//       // });
+//     }
+//   };
 
-const searchInput = document.querySelector(".search-input");
-const searchBtn = document.querySelector(".search-btn");
-const searchForm = document.querySelector(".search-form");
+//   xmlhttp.open(
+//     "GET",
+//     "./includes/student.inc.php?query&status=" +
+//       statusValue +
+//       "&row=" +
+//       rowValue +
+//       "&page_no=" +
+//       page,
+//     true
+//   );
+//   xmlhttp.send();
+// };
 
-let query;
-query = searchInput.value;
+// // const searchInput = document.querySelector(".search-input");
+// // const searchBtn = document.querySelector(".search-btn");
+// // const searchForm = document.querySelector(".search-form");
+// // const statusSelect = document.querySelector(".status-select");
+// // const rowSelect = document.querySelector(".row-select");
 
-const statusSelect = document.querySelector(".status-select");
+// // let query = searchInput.value;
+// // let statusValue = statusSelect.value;
+// // let rowValue = rowSelect.value;
 
-let statusValue;
-statusValue = statusSelect.value;
+// // Search query
+// // searchForm.addEventListener("submit", (e) => {
+// //   e.preventDefault();
+// // });
 
-// Search query
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+// // searchInput.addEventListener("keyup", (e) => {
+// //   query = e.target.value;
+// //   loadMasterList(query);
+// // });
 
-searchInput.addEventListener("keyup", (e) => {
-  query = e.target.value;
-  loadMasterList(query, statusValue);
-});
+// // searchBtn.addEventListener("click", () => {
+// //   query = searchInput.value;
+// //   loadMasterList(query);
+// // });
 
-searchBtn.addEventListener("click", () => {
-  query = searchInput.value;
-  loadMasterList(query, statusValue);
-});
+// // statusSelect.addEventListener("change", () => {
+// //   statusValue = statusSelect.value;
+// //   loadMasterList(query, statusValue);
+// // });
 
-statusSelect.addEventListener("change", () => {
-  statusValue = statusSelect.value;
-  loadMasterList(query, statusValue);
-});
+// // rowSelect.addEventListener("change", () => {
+// //   rowValue = rowSelect.value;
+// //   loadMasterList(query, statusValue, rowValue);
+// // });
 
-loadMasterList(query, statusValue);
+// loadMasterList();
+
+// // console.log(previousPageBtn);
