0fd172db848f809e03688c4ff39f440841f25283
Justine <ujusine@yahoo.com>
2023-04-27 10:02:25 +0800

    update enrollment form

--- classes/Models/enrollment.class.php
+++ classes/Models/enrollment.class.php
@@ -35,8 +35,8 @@ class Enrollment extends \Dbh{
             // Query
             $sql = 
             "INSERT INTO `students_table` (surname, first_name, middle_name, ext, `image`, lrn,
-            from_sy, to_sy, birth_date, gender, religion, house_street, subdivision, barangay, city, province, region)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+            birth_date, gender, religion, house_street, subdivision, barangay, city, province, region)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
             INSERT INTO `fathers_table` (student_lrn, father_surname, father_first_name, father_middle_name, father_education, father_employment, father_contact_number, is_beneficiary)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?);
             INSERT INTO `mothers_table` (student_lrn, mother_surname, mother_first_name, mother_middle_name, mother_education, mother_employment, mother_contact_number, is_beneficiary)
@@ -48,13 +48,85 @@ class Enrollment extends \Dbh{
             ";
             // Execute
             $stmt = $this->connection()->prepare($sql);
-            $stmt->execute([$sname, $fname, $mname, $extname, $base64_image, $lrn, $from_sy, $to_sy, $bdate, $gender, $religion, $house_street, $subdivision, $barangay, $city, $province, $region,
+            $stmt->execute([$sname, $fname, $mname, $extname, $base64_image, $lrn, $bdate, $gender, $religion, $house_street, $subdivision, $barangay, $city, $province, $region,
             $lrn, $father_surname, $father_fname, $father_mname, $father_education, $father_employment, $father_contact, $is_beneficiary,
             $lrn, $mother_surname, $mother_fname, $mother_mname, $mother_education, $mother_employment, $mother_contact, $is_beneficiary,
             $lrn, $guardian_surname, $guardian_fname, $guardian_mname, $guardian_education, $guardian_employment, $guardian_contact, $is_beneficiary,
             $lrn, $from_sy, $to_sy, $school, $grade_lvl, $section, $status]);
             $stmt = null;
 
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+
+    protected function update($curr_lrn, $enrollment_id, $student_id, $sname, $fname, $mname, $extname, $lrn, $from_sy, $to_sy, $grade_lvl, $section, $file, $bdate, $gender, $religion, 
+    $house_street, $subdivision, $barangay, $city, $province, $region,
+    $father_surname, $father_fname, $father_mname, $father_education, 
+    $father_employment, $father_contact, $mother_surname, $mother_fname, 
+    $mother_mname, $mother_education, $mother_employment, $mother_contact,
+    $guardian_surname, $guardian_fname, $guardian_mname, $guardian_education, 
+    $guardian_employment, $guardian_contact, $is_beneficiary,
+    $father_education_textbox, $mother_education_textbox, $guardian_education_textbox){
+        try{
+            $image = $file['tmp_name'];
+            $base64_image = base64_encode(file_get_contents(addslashes($image)));
+
+            $school = "Sabang Elementary School";
+            $status = "Active";
+            // Change value
+            if ($father_education === "Others") {
+                $father_education = $father_education_textbox;
+            }
+
+            if ($mother_education === "Others") {
+                $mother_education = $mother_education_textbox;
+            }
+
+            if ($guardian_education === "Others") {
+                $guardian_education = $guardian_education_textbox;
+            }
+            // Query
+            $sql = 
+            "UPDATE `students_table` SET surname = ?, first_name = ?, middle_name = ?, ext = ?, `image` = ?, lrn = ?, birth_date = ?, gender = ?, religion = ?, house_street = ?, subdivision = ?, barangay = ?, city = ?, province = ?, region = ?
+            WHERE `student_id` = ?;
+            UPDATE `fathers_table` SET student_lrn = ?, father_surname = ?, father_first_name = ?, father_middle_name = ?, father_education = ?, father_employment = ?, father_contact_number = ?, is_beneficiary = ?
+            WHERE `student_lrn` = ?;
+            UPDATE `mothers_table` SET student_lrn = ?, mother_surname = ?, mother_first_name = ?, mother_middle_name = ?, mother_education = ?, mother_employment = ?, mother_contact_number = ?, is_beneficiary = ?
+            WHERE `student_lrn` = ?;
+            UPDATE `guardians_table` SET student_lrn = ?, guardian_surname = ?, guardian_first_name = ?, guardian_middle_name = ?, guardian_education = ?, guardian_employment = ?, guardian_contact_number = ?, is_beneficiary = ?
+            WHERE `student_lrn` = ?;
+            UPDATE `enrollment_history_table` SET student_lrn = ?, from_sy = ?, to_sy = ?, school = ?, grade_level = ?, section = ?, `status` = ?
+            WHERE `enrollment_id` = ?;
+
+            ";
+            // Execute
+            $stmt = $this->connection()->prepare($sql);
+            $stmt->execute([$sname, $fname, $mname, $extname, $base64_image, $lrn, $bdate, $gender, $religion, $house_street, $subdivision, $barangay, $city, $province, $region, $student_id,
+            $lrn, $father_surname, $father_fname, $father_mname, $father_education, $father_employment, $father_contact, $is_beneficiary, $curr_lrn,
+            $lrn, $mother_surname, $mother_fname, $mother_mname, $mother_education, $mother_employment, $mother_contact, $is_beneficiary, $curr_lrn,
+            $lrn, $guardian_surname, $guardian_fname, $guardian_mname, $guardian_education, $guardian_employment, $guardian_contact, $is_beneficiary, $curr_lrn,
+            $lrn, $from_sy, $to_sy, $school, $grade_lvl, $section, $status, $enrollment_id]);
+            $stmt = null;
+
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+
+    }
+
+    protected function checkGrades($curr_lrn, $grade_level, $section){
+        try{
+            $sql = "SELECT * FROM student_grades_table WHERE student_lrn = ? AND grade_level = ? AND section = ?;";
+            $stmt = $this->connection()->prepare($sql);
+            $stmt->execute([$curr_lrn, $grade_level, $section]);
+    
+            $results = $stmt->fetchAll();
+            return $results;
         }
         catch(PDOException $e) {
             echo "Error: " . $e->getMessage();
