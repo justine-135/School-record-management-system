909d33397e49b5ead615b656c50179a4ea57920a
Justine <ujusine@yahoo.com>
2023-04-27 10:03:06 +0800

    get post data for update

--- includes/enrollment.inc.php
+++ includes/enrollment.inc.php
@@ -1,13 +1,21 @@
 <?php
 
-require '../classes/Controllers/enrollment.class.php';
-require '../classes/Controllers/enrollment_history.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/sabanges/classes/Controllers/enrollment_history.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/sabanges/classes/Controllers/enrollment.class.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/sabanges/classes/Views/student.class.php';
 
 use Controllers\EnrollmentController;
 use Controllers\EnrollmentHistoryController;
+use Views\StudentInformationView;
+
+// function enrollment_form_obj($edit_id){
+//     $view_form_obj = new StudentInformationView();
+//     $view_form_obj->viewStudentForm($edit_id);
+
+//     return $view_form_obj;
+// }
 
 if (isset($_POST["enroll"])) {
-
     // Student information
     $sname = $_POST['sname'];
     $fname = $_POST['fname'];
@@ -90,6 +98,85 @@ elseif (isset($_POST['add-enrollment-history'])) {
 
     $enrollment_history_check_obj = new EnrollmentHistoryController();
     $enrollment_history_validation_result = $enrollment_history_check_obj->checkValidationHistory($id, $lrn, $from_sy, $to_sy, $old_grade_lvl, $grade_lvl, $section, $status);
+}
+elseif (isset($_POST['update'])) {
+    // Student information
+    $curr_lrn = $_POST['curr-lrn'];
+    $curr_grade_lvl = $_POST['curr-grade-level'];
+    $curr_section = $_POST['curr-section'];
+    $enrollment_id = $_POST['enrollment_id'];
+    $student_id = $_POST['student_id'];
+
+    $sname = $_POST['sname'];
+    $fname = $_POST['fname'];
+    $mname = $_POST['mname'];
+    $extname = $_POST['extname'];
+    $lrn = $_POST['lrn'];
+    $from_sy = $_POST['from-sy'];
+    $to_sy = $_POST['to-sy'];
+    $grade_lvl = $_POST['grade-lvl'];
+    $section = $_POST['section'];
+    $file = $_FILES['file'];
+    $bdate = $_POST['birth-date'];
+    $gender = $_POST['gender'];
+    $religion = $_POST['religion'];
+    $house_street = $_POST['house-number-street'];
+    $subdivision = $_POST['subdv-village-zone'];
+    $barangay = $_POST['barangay'];
+    $city = $_POST['city-municipality'];
+    $province = $_POST['province'];
+    $region = $_POST['region'];
+
+    // Father information
+    $father_surname = $_POST['f-surname'];
+    $father_fname = $_POST['f-fname'];
+    $father_mname = $_POST['f-mname'];
+    $father_education = $_POST['f-highest-education'];
+
+    $father_employment = $_POST['f-employment-status'];
+    $father_contact = $_POST['f-contact-number'];
+    $is_beneficiary = $_POST['is-beneficiary'];
+
+    // Mother information
+    $mother_surname = $_POST['m-surname'];
+    $mother_fname = $_POST['m-fname'];
+    $mother_mname = $_POST['m-mname'];
+    $mother_education = $_POST['m-highest-education'];
+
+    $mother_relation = "Mother";
+    $mother_employment = $_POST['m-employment-status'];
+    $mother_contact = $_POST['m-contact-number'];
+
+    // Guardian information
+    $guardian_surname = $_POST['g-surname'];
+    $guardian_fname = $_POST['g-fname'];
+    $guardian_mname = $_POST['g-mname'];
+    $guardian_education = $_POST['g-highest-education'];
+
+    $guardian_relation = "Guardian";
+    $guardian_employment = $_POST['g-employment-status'];
+    $guardian_contact = $_POST['g-contact-number'];
+    $father_education_textbox = null; 
+    $mother_education_textbox = null;
+    $guardian_education_textbox = null;
+    var_dump($guardian_employment);
+
+    // Check inputs
+    $enrollment_check_obj = new EnrollmentController();
+    $enrollment_validation_result = $enrollment_check_obj->initUpdate(
+    $curr_lrn, $curr_grade_lvl, $curr_section, $enrollment_id, $student_id, $sname, $fname, $mname, $extname, $lrn, $from_sy, $to_sy, $grade_lvl, $section, $file, $bdate, $gender, $religion, 
+    $house_street, $subdivision, $barangay, $city, $province, $region,
+    $father_surname, $father_fname, $father_mname, $father_education, $father_employment, $father_contact, 
+    $mother_surname, $mother_fname, $mother_mname, $mother_education, $mother_employment, $mother_contact,
+    $guardian_surname, $guardian_fname, $guardian_mname, $guardian_education, $guardian_employment, $guardian_contact, 
+    $is_beneficiary,$father_education_textbox, $mother_education_textbox, $guardian_education_textbox);
+
+    // echo $sname, $fname, $mname, $extname, $lrn, $from_sy, $to_sy, $grade_lvl, $section, $file, $bdate, $gender, $religion, 
+    // $house_street, $subdivision, $barangay, $city, $province, $region,
+    // $father_surname, $father_fname, $father_mname, $father_education, $father_employment, $father_contact, 
+    // $mother_surname, $mother_fname, $mother_mname, $mother_education, $mother_employment, $mother_contact,
+    // $guardian_surname, $guardian_fname, $guardian_mname, $guardian_education, $guardian_employment, $guardian_contact, 
+    // $is_beneficiary;
 }
 else{
     header("location: ../index.php");
