7cc9fd6a510c381764eebc8a732e53431dd583e5
Justine <ujusine@yahoo.com>
2023-05-02 16:48:46 +0800

    compute final grade for adding grade

--- js/grading.js
+++ js/grading.js
@@ -1,3 +1,4 @@
+// Open grade modal and load grade form
 const openGradeBtns = document.querySelectorAll(".open-grade-btn");
 
 openGradeBtns.forEach((element) => {
@@ -12,6 +13,82 @@ openGradeBtns.forEach((element) => {
         // Show HTML
         const masterlistTable = document.querySelector(".grading-modal-body");
         masterlistTable.innerHTML = this.responseText;
+
+        // Compute final grade
+        const finalGradesInput = document.querySelectorAll(".final-grade");
+        const reviewBtn = document.querySelectorAll(".review-grades");
+
+        reviewBtn.forEach((element) => {
+          element.addEventListener("click", () => {
+            finalGradesInput.forEach((element) => {
+              let quarter1 =
+                element.parentElement.parentElement.childNodes[3].childNodes[1]
+                  .value.length == 0
+                  ? (element.parentElement.parentElement.childNodes[3].childNodes[1].value = 60)
+                  : element.parentElement.parentElement.childNodes[3]
+                      .childNodes[1].value;
+              let quarter2 =
+                element.parentElement.parentElement.childNodes[5].childNodes[1]
+                  .value.length == 0
+                  ? (element.parentElement.parentElement.childNodes[5].childNodes[1].value = 60)
+                  : element.parentElement.parentElement.childNodes[5]
+                      .childNodes[1].value;
+              let quarter3 =
+                element.parentElement.parentElement.childNodes[7].childNodes[1]
+                  .value.length == 0
+                  ? (element.parentElement.parentElement.childNodes[7].childNodes[1].value = 60)
+                  : element.parentElement.parentElement.childNodes[7]
+                      .childNodes[1].value;
+              let quarter4 =
+                element.parentElement.parentElement.childNodes[9].childNodes[1]
+                  .value.length == 0
+                  ? (element.parentElement.parentElement.childNodes[9].childNodes[1].value = 60)
+                  : element.parentElement.parentElement.childNodes[9]
+                      .childNodes[1].value;
+
+              // Mean
+              let dividedTo = 4;
+
+              if (quarter1 === "N/A") {
+                quarter1 = 0;
+                dividedTo--;
+              }
+
+              if (quarter2 === "N/A") {
+                quarter2 = 0;
+                dividedTo--;
+              }
+
+              if (quarter3 === "N/A") {
+                quarter3 = 0;
+                dividedTo--;
+              }
+
+              if (quarter4 === "N/A") {
+                quarter4 = 0;
+                dividedTo--;
+              }
+
+              console.log(quarter1);
+              console.log(quarter2);
+              console.log(quarter3);
+              console.log(quarter4);
+
+              let finalGrade =
+                (parseFloat(quarter1) +
+                  parseFloat(quarter2) +
+                  parseFloat(quarter3) +
+                  parseFloat(quarter4)) /
+                dividedTo;
+
+              if (isNaN(finalGrade)) {
+                element.value = "Incomplete";
+              } else {
+                element.value = finalGrade.toFixed(2);
+              }
+            });
+          });
+        });
       }
     };
 
