00df45eca9da2420827983d9e94afb9cca05e9d0
Justine <ujusine@yahoo.com>
2023-05-02 16:49:08 +0800

    update

--- includes/grades.inc.php
+++ includes/grades.inc.php
@@ -6,9 +6,14 @@ require '../classes/Views/grades.class.php';
 use Controllers\GradesController;
 use Views\GradesView;
 
-function initAddGrades($id, $lrn, $grade_level, $section, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter){
+function initAddGrades($id, $lrn, $grade_level, $section, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter, $rows, $status, $page_no){
     $obj = new GradesController();
-    $obj->initCreate($id, $lrn, $grade_level, $section, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter);
+    $obj->initCreate($id, $lrn, $grade_level, $section, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter, $rows, $status, $page_no);
+}
+
+function initUpdateGrades($id, $lrn, $grade_level, $section, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter, $rows, $status, $page_no){
+    $obj = new GradesController();
+    $obj->initUpdate($id, $lrn, $grade_level, $section, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter, $rows, $status, $page_no);
 }
 
 function initLoadGrades($lrn){
@@ -17,6 +22,9 @@ function initLoadGrades($lrn){
 }
 
 if (isset($_POST["submit-grade"])) {
+    $rows = $_POST['rows'];
+    $status = $_POST['status'];
+    $page_no = $_POST['page-no'];
     $id = isset($_POST['id']) ? $_POST['id'] : '';
     $lrn = isset($_POST['lrn']) ? $_POST['lrn'] : '';
     $subjects = isset($_POST['subjects']) ? $_POST['subjects'] : array("");
@@ -27,7 +35,24 @@ if (isset($_POST["submit-grade"])) {
     $grade_level = isset($_POST['grade-level']) ? $_POST['grade-level'] : '';
     $section = $_POST['section'];
 
-    initAddGrades($id, $lrn, $grade_level, $section, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter);
+    initAddGrades($id, $lrn, $grade_level, $section, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter, $rows, $status, $page_no);
+}
+
+if (isset($_POST['update-grade'])) {
+    $rows = $_POST['rows'];
+    $status = $_POST['status'];
+    $page_no = $_POST['page-no'];
+    $id = isset($_POST['id']) ? $_POST['id'] : '';
+    $lrn = isset($_POST['lrn']) ? $_POST['lrn'] : '';
+    $subjects = isset($_POST['subjects']) ? $_POST['subjects'] : array("");
+    $first_quarter = isset($_POST['first-quarter']) ? $_POST['first-quarter'] : array();
+    $second_quarter = isset($_POST['second-quarter']) ? $_POST['second-quarter'] : array();
+    $third_quarter = isset($_POST['third-quarter']) ? $_POST['third-quarter'] : array();
+    $fourth_quarter = isset($_POST['fourth-quarter']) ? $_POST['fourth-quarter'] : array();
+    $grade_level = isset($_POST['grade-level']) ? $_POST['grade-level'] : '';
+    $section = $_POST['section'];
+
+    initUpdateGrades($id, $lrn, $grade_level, $section, $subjects, $first_quarter, $second_quarter, $third_quarter, $fourth_quarter, $rows, $status, $page_no);
 }
 
 if (isset($_GET['load_grade'])) {
