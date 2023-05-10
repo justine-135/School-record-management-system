75e18d98e4bd18544201726205548c2ddc3c1e8b
Justine <ujusine@yahoo.com>
2023-05-10 13:50:13 +0800

    update

--- login.php
+++ login.php
@@ -13,20 +13,18 @@ if (!empty($_SESSION['username']) && !empty($_SESSION['account_id'])) {
             <div class="mb-3 px-3">
                 <label for="username-email" class="form-label fw-semibold">Username</label>
                 <input type="text" class="form-control" name="username" id="username-email" placeholder="Enter username or email ...">
-                <div id="emailHelp" class="form-text">*Default username is: (lastname12345).</div>
-
             </div>
             <div class="mb-3 px-3">
                 <label for="password" class="form-label fw-semibold">Password</label>
                 <input type="password" class="form-control" name="password" id="password" placeholder="Enter password ...">
-                <div id="emailHelp" class="form-text">*Default password is date of birth.</div>
+                <div id="emailHelp" class="form-text">*Default password is: (SURNAME12345).</div>
             </div>
             <hr>
             <div class="mb-3 px-3">
                 <input type="submit" class="form-control btn btn-primary" name="login" id="password" value="Login">
             </div>
             <div class="mb-3 px-3 text-center">
-                <a href="change_password.php">Change password</a>
+                <a href="changepass.php">Change password</a>
             </div>
         </form>
     </div>
