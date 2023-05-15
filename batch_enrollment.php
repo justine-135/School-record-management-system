1df91b6a630e02d4675463012f208e2593a7c6ea
Justine <ujusine@yahoo.com>
2023-05-15 16:58:31 +0800

    update

--- batch_enrollment.php
+++ batch_enrollment.php
@@ -14,17 +14,10 @@ if (empty($_SESSION['username']) && empty($_SESSION['account_id'])) {
 ?>
 
 <main class="container-fluid w-90 mt-4 ">
-  <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="enrollment.php">New student</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link active" href="batch_enrollment.php">Batch enrollment</a>
-        </li>
-    </ul>
+    <?php include './partials/nav_enrollment_tabs.php'; ?>
     <form class="border border-top-0 p-3 bg-white mb-3" action="./includes/enrollment.inc.php" method="post" enctype="multipart/form-data">
     <?php
-        require $_SERVER['DOCUMENT_ROOT'].'/sabanges/includes/student.inc.php';
+        require $_SERVER['DOCUMENT_ROOT'].'/sabanges/includes/enrollment.inc.php';
     ?>      
     </form>
 </main>
