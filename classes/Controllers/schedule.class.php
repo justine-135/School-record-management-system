21c62940cb1095e7eb3de23a289f4f520c76c064
Justine <ujusine@yahoo.com>
2023-05-18 15:46:13 +0800

    create and delete schedule

---
+++ classes/Controllers/schedule.class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Controllers;
+
+include $_SERVER['DOCUMENT_ROOT'].'/sabanges/classes/Models/schedule.class.php';
+
+class ScheduleController extends \Models\Schedule{
+    public function initCreate($start, $end){
+        $this->create($start, $end);
+    }
+    public function initDestroy($id){
+        $this->destroy($id);
+        header("Location: ../operations_grading.php?id=" . $id . "&operations_grading&deleted");
+        die();
+    }
+}
