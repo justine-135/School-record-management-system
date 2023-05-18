dc7aaa98d3e46c0f8705412a7b56f61ebc8eb604
Justine <ujusine@yahoo.com>
2023-05-18 15:52:34 +0800

    show grade levels and ssections added

--- classes/Views/grade_levels.class.php
+++ classes/Views/grade_levels.class.php
@@ -13,7 +13,6 @@ class GradeLevelsView extends \Models\GradeLevels{
     }
     public function initIndexSelectEnrollmentEdit($section_params){
         $results = $this->indexSelect($section_params);
-        var_dump($results);
         foreach ($results as $row) { ?>
         <option value="<?= $row['section'] ?>" ><?= $row['section'] ?></option>
             <?php
@@ -28,9 +27,10 @@ class GradeLevelsView extends \Models\GradeLevels{
         $level = isset($_GET['level']) ? $_GET['level'] : "";
         $section = isset($_GET['section']) ? $_GET['section'] : "";
         $results = $this->indexSelect($section_params);
+        
         ?>
         <li>
-            <a class="dropdown-item" href="?row=<?= $rows ?>&page_no=<?= $page_no ?>&status=<?= $status ?>&level=<?= $level ?>&section=None&query=<?= $query ?>" >None</a>
+            <a class="dropdown-item" href="?row=<?= $rows ?>&page_no=<?= $page_no ?>&status=<?= $status ?>&level=<?= $level ?>&section=&query=<?= $query ?>" >All</a>
         </li>
         <?php
         foreach ($results as $row) { ?>
@@ -42,37 +42,74 @@ class GradeLevelsView extends \Models\GradeLevels{
     }
 
     public function initIndex(){
-        $results = $this->index();
-        ?>
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Added at</th>
-                <th scope="col">Updated at</th>
-                <th scope="col">Grade level</th>
-                <th scope="col">Section</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php
-        foreach ($results as $row) {
-        ?>
-            <tr>
-                <th scope="row"><?= $row['id'] ?></th>
-                <td><?= $row['added_at'] ?></td>
-                <td><?= $row['updated_at'] ?></td>
-                <td><?= $row['grade'] ?></td>
-                <td><?= $row['section'] ?></td>
-                <td>
-                    <button class="btn btn-primary">Edit</button>
-                    <button class="btn btn-danger">Delete </button>
-                </td>
-            </tr>
-        <?php
-        }
-        ?>
-        </tbody>
+        $rows = isset($_GET['row']) ? $_GET['row'] : '10';
+        $page_no = isset($_GET['page_no']) ? $_GET['page_no'] : '1';
+        $level = isset($_GET['level']) ? $_GET['level'] : "";
+
+        $page_no = intval($page_no);
+        $total_records_per_page = $rows;
+        $offset = ($page_no - 1) * intval($total_records_per_page);
+        $previous_page = $page_no - 1;
+        $next_page = $page_no + 1;
+
+        $result_count = $this->sectionsCount($level);
+        $records = count($result_count);
+
+        $total_no_page = ceil($records / intval($total_records_per_page));
+        $results = $this->index($level, $offset, $total_records_per_page);
+
+
+    ?>
+            <table class="table table-hover border mb-0 mt-2 grade-levels-table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Grade level</th>
+                        <th scope="col">Section</th>
+                        <th scope="col">Added at</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($results as $row) {
+                ?>
+                    <tr>
+                        <th scope="row"><?= $row['id'] ?></th>
+                        <td><?= $row['grade'] ?></td>
+                        <td><?= $row['section'] ?></td>
+                        <td><?= $row['added_at'] ?></td>
+                        <td>
+                            <form action="./includes/operations.inc.php" method="post" enctype="multipart/form-data">
+                                <input type="text" name="id" value="<?= $row['id'] ?>" id="" hidden>
+                                <button type="submit" name="delete-section" class="btn btn-danger">Delete </button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
+                </tbody>
+            </table>
+            <nav class="m-2">
+                <ul class="pagination">
+                    <li class="page-item">
+                        
+                        <a class="page-link previous-btn <?= $page_no <= 1 ? 'disabled' : '' ?>" href="?row=<?= $rows ?>&page_no=<?= $previous_page ?>&level=<?= $level ?>">Previous</a>
+                    </li>
+                    <?php for ($i=0; $i < $total_no_page; $i++) { ?>
+
+                    <li class="page-item">
+                        <a class="page-link page-number <?= $page_no !== $i + 1 ? '' : 'active'?>" href="?row=<?= $rows ?>&page_no=<?= $i + 1 ?>&level=<?= $level ?>"><?= $i + 1?></a>
+                    </li>
+                
+                    <?php } ?>
+                    <li class="page-item">
+                        <a class="page-link next-btn <?= $page_no >= $total_no_page ? 'disabled' : '' ?>" href="?row=<?= $rows ?>&page_no=<?= $next_page ?>status=<?= $status ?>&level=<?= $level ?>">Next</a>
+                    </li>
+                </ul>
+                <span class="fw-semibold">Page <?= $page_no ?> out of <?= $total_no_page ?></span>
+            </nav>
         <?php
     }
 }
