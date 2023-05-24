d3f67f7673a223f518b484ede4bdfb2f7b5ad4f5
Justine <ujusine@yahoo.com>
2023-05-24 16:37:55 +0800

    session destroy on logout

--- includes/teachers.inc.php
+++ includes/teachers.inc.php
@@ -9,15 +9,11 @@ use Views\TeacherInformationView;
 
 function register($surname, $first_name, $middle_name, $ext_name, $birth_date, $gender, $contact, 
 $religion, $house_street, $subdivision, $barangay, $city, $province, $region, $email, $file,
-$permission_1, $permission_2, $permission_3, $permission_4, $permission_5,
-$permission_6, $permission_7, $permission_8, $permission_9, $permission_10, $permission_11,
-$permission_12, $permission_13, $permission_14, $permission_15, $permission_16, $grade_level, $section){
+$permission, $grade_level, $section){
     $obj = new TeachersController();
     $obj->initCreate($surname, $first_name, $middle_name, $ext_name, $birth_date, $gender, $contact, 
     $religion, $house_street, $subdivision, $barangay, $city, $province, $region, $email, $file,
-    $permission_1, $permission_2, $permission_3, $permission_4, $permission_5,
-    $permission_6, $permission_7, $permission_8, $permission_9, $permission_10, $permission_11,
-    $permission_12, $permission_13, $permission_14, $permission_15, $permission_16, $grade_level, $section);
+    $permission, $grade_level, $section);
 }
 
 function index(){
@@ -56,31 +52,14 @@ if (isset($_POST['register'])) {
     // $password = $_POST['password'];    
     // $confirm_password = $_POST['confirm_password'];    
 
-    $permission_1 = isset($_POST['masterlist-view']) ? $_POST['masterlist-view'] : 0;
-    $permission_2 = isset($_POST['masterlist-promotion-retention']) ? $_POST['masterlist-promotion-retention'] : 0;
-    $permission_3 = isset($_POST['student-view']) ? $_POST['student-view'] : 0;
-    $permission_4 = isset($_POST['student-edit']) ? $_POST['student-edit'] : 0;
-    $permission_5 = isset($_POST['student-history']) ? $_POST['student-history'] : 0;
-    $permission_6 = isset($_POST['student-grades']) ? $_POST['student-grades'] : 0;
-    $permission_7 = isset($_POST['enrollment-view']) ? $_POST['enrollment-view'] : 0;
-    $permission_8 = isset($_POST['enrollment-add']) ? $_POST['enrollment-add'] : 0;
-    $permission_9 = isset($_POST['teachers-view']) ? $_POST['teachers-view'] : 0;
-    $permission_10 = isset($_POST['teachers-add']) ? $_POST['teachers-add'] : 0;
-    $permission_11 = isset($_POST['teachers-edit']) ? $_POST['teachers-edit'] : 0;
-    $permission_12 = isset($_POST['teacher-view']) ? $_POST['teacher-view'] : 0;
-    $permission_13 = isset($_POST['teacher-edit']) ? $_POST['teacher-edit'] : 0;
-    $permission_14 = isset($_POST['operations-view']) ? $_POST['operations-view'] : 0;
-    $permission_15 = isset($_POST['operations-add']) ? $_POST['operations-add'] : 0;
-    $permission_16 = isset($_POST['operations-edit']) ? $_POST['operations-edit'] : 0;
+    $permission = $_POST['role'];
 
     $grade_level = isset($_POST['grade-level']) ? $_POST['grade-level'] : null;
     $section = isset($_POST['section']) ? $_POST['section'] : null;
 
     register($surname, $first_name, $middle_name, $ext_name, $birth_date, $gender, $contact, $religion, 
     $house_street, $subdivision, $barangay, $city, $province, $region, 
-    $email, $file, $permission_1, $permission_2, $permission_3, $permission_4, $permission_5,
-    $permission_6, $permission_7, $permission_8, $permission_9, $permission_10, $permission_11,
-    $permission_12, $permission_13, $permission_14, $permission_15, $permission_16, $grade_level, $section);
+    $email, $file, $permission, $grade_level, $section);
     // register($surname, $first_name, $middle_name, $ext_name, $birth_date, $gender, $contact, $religion, $house_street, $subdivision, $barangay, $city, $province, $region, $email, $file);
 }
 
@@ -125,8 +104,7 @@ if (isset($_POST['login'])) {
 
 if (isset($_POST['logout'])) {
     session_start();
-    unset($_SESSION["username"]);
-    unset($_SESSION["account_id"]);
+    session_destroy();
     header("Location: ../index.php");
 }
 
