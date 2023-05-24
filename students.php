2257754151a33bd277e8a1da78d09e64910c2875
Justine <ujusine@yahoo.com>
2023-05-24 16:36:24 +0800

    added permission and session time

--- students.php
+++ students.php
@@ -8,17 +8,14 @@
 <?php include './partials/alert.php'; ?>
 
 <?php
-if (empty($_SESSION['username']) && empty($_SESSION['account_id'])) {
-  header("Location: ./login.php");
-}
+$_SESSION['page_permission'] = 'teacher';
+include './includes/session.inc.php';
+include './includes/permission.inc.php';
 ?>
 
 <main class="container-fluid w-90 mt-4 ">
   <?php include './partials/nav_records_tabs.php'; ?>
   <div class="p-2 border border-top-0">
-    <div class="d-flex align-items-center justify-content-between">
-      <h5>All students</h5>              
-    </div>
     <div>
       <?php include './partials/nav_filter_student.php'; ?>
     </div>
