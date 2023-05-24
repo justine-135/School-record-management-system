b5e6c12b12cefc6dc62de7f78533497f8eca60d8
Justine <ujusine@yahoo.com>
2023-05-24 16:36:01 +0800

    added permission and session time

--- operations_subjects.php
+++ operations_subjects.php
@@ -8,9 +8,9 @@
 <?php include './partials/alert.php'; ?>
 
 <?php
-if (empty($_SESSION['username']) && empty($_SESSION['account_id'])) {
-  header("Location: ./login.php");
-}
+$_SESSION['page_permission'] = 'author';
+include './includes/session.inc.php';
+include './includes/permission.inc.php';
 ?>
 
 <?php include './partials/subjects_modal.php'; ?>
