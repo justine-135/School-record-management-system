b5045ccc5721dc8f735d858b639577aa78a7d834
Justine <ujusine@yahoo.com>
2023-05-24 16:37:13 +0800

    permission files for user types

---
+++ includes/permission.inc.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SESSION['is_superadmin'] == 0) {
+        switch ($_SESSION['page_permission']) {
+            case 'admin':
+                if ($_SESSION['is_admin'] == 0) {
+                    header("Location: ./index.php?permission&err");
+                    die();
+                }
+                break;
+            case 'teacher':
+                if ($_SESSION['is_teacher'] == 0) {
+                    header("Location: ./index.php?permission&err");
+                    die();
+                }
+                break;
+            case 'guidance':
+                if ($_SESSION['is_guidance'] == 0) {
+                    header("Location: ./index.php?permission&err");
+                    die();
+                }
+                break;
+            case 'author':
+                if ($_SESSION['is_author'] == 0) {
+                    header("Location: ./index.php?permission&err");
+                    die();
+                }
+                break;
+            default:
+                break;
+        }
+    
+}  
