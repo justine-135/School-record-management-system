07bcac6ba7452b7d96b54353dc658f56b307ccde
Justine <ujusine@yahoo.com>
2023-05-24 16:35:17 +0800

    added permission and session time

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 <?php include "partials/nav.php"; ?>
 
 <?php
-if (empty($_SESSION['username']) && empty($_SESSION['account_id'])) {
-  header("Location: ./login.php");
-}
+include './includes/session.inc.php';
 ?>
 
-<main class="container-fluid w-90 border mt-4 p-4 bg-white">
-  <h4>Home</h4>
+<main class="container-fluid w-90 border mt-4 p-0 bg-white">
+  <div class="p-2 border-bottom">
+    <h5>Welcome <?= ucfirst($_SESSION['surname']) . ', ' . ucfirst($_SESSION['first_name']) . ' ' . ucfirst($_SESSION['middle_name']); ?></h5>
+  </div>
   <p class="">Welcome to homepage</p>
 </main>
 <?php require $_SERVER['DOCUMENT_ROOT'].'/sabanges/partials/footer.php'; ?>
