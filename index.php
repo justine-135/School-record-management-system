64d0a8b73a14588400f7e6f2a6f7d705396a5c18
Justine <ujusine@yahoo.com>
2023-05-26 15:05:00 +0800

    load dashboard

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php include "partials/header.php"; ?>
 
 <?php $header = "/"; ?>
+<?php $view = "index"; ?>
 
 <?php include "partials/nav.php"; ?>
+<?php include './partials/alert.php'; ?>
 
 <?php
 include './includes/session.inc.php';
 ?>
 
 <main class="container-fluid w-90 border mt-4 p-0 bg-white">
-  <div class="p-2 border-bottom">
-    <h5>Welcome <?= ucfirst($_SESSION['surname']) . ', ' . ucfirst($_SESSION['first_name']) . ' ' . ucfirst($_SESSION['middle_name']); ?></h5>
+  <div class="d-flex justify-content-between align-items-center p-2 border-bottom">
+    <h5>Welcome user # <?= $_SESSION['account_id']; ?></h5>
+    <h6>Role: <?= $_SESSION['is_superadmin'] == 1 ? 'Superadmin' : ($_SESSION['is_admin'] == 1 ? 'Admin' : ($_SESSION['is_teacher'] == 1 ? 'Teacher' : ($_SESSION['is_guidance'] == 1 ? 'Guidance' : ($_SESSION['author'] == 1 ? 'Author' : 'None')))) ?></h6>
   </div>
-  <p class="">Welcome to homepage</p>
+  <div>
+  <?php require $_SERVER['DOCUMENT_ROOT'].'/sabanges/includes/teachers.inc.php'; ?>
+  </div>
 </main>
+
+<script src="js/index.js"></script>
 <?php require $_SERVER['DOCUMENT_ROOT'].'/sabanges/partials/footer.php'; ?>
