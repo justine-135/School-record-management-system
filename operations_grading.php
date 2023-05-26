a538e71fc3c906886dd85e42e027360c34799c61
Justine <ujusine@yahoo.com>
2023-05-26 15:05:12 +0800

    update

--- operations_grading.php
+++ operations_grading.php
@@ -19,11 +19,7 @@ include './includes/permission.inc.php';
   <?php include './partials/nav_operation_tabs.php'; ?>
   <div class="p-2 border border-top-0">
     <div class="d-flex align-items-center justify-content-between">
-      <h5>Schedule grading period</h5>
-      <button type="button" class="btn btn-primary d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#scheduleModal">
-      Schedule
-      <?php require './partials/add_icon.php' ?>
-      </button>                
+             
     </div>
     <div>
       <?php include './partials/nav_filter_operations.php'; ?>
