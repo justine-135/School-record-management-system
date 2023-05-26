8eea8416300913ff8bb3819ae42326805c42617b
Justine <ujusine@yahoo.com>
2023-05-26 15:05:26 +0800

    update

--- operations_subjects.php
+++ operations_subjects.php
@@ -19,11 +19,7 @@ include './includes/permission.inc.php';
   <?php include './partials/nav_operation_tabs.php'; ?>
   <div class="p-2 border border-top-0">
     <div class="d-flex align-items-center justify-content-between">
-      <h5>Manage subjects</h5>
-      <button type="button" class="btn btn-primary d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#subjectsModal">
-      Add
-      <?php require './partials/add_icon.php' ?>
-      </button>                
+              
     </div>
     <div>
       <?php include './partials/nav_filter_operations.php'; ?>
