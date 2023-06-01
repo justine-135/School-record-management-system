637ad5ffb58f4851be4926c9cf1badc9a5bbe173
Justine <ujusine@yahoo.com>
2023-06-01 10:33:32 +0800

    updated

--- student_informations.php
+++ student_informations.php
@@ -4,7 +4,7 @@
 <?php $view="student-informations"; ?>
 
 <?php include "partials/nav.php"; ?>
-<?php include 'partials/alert.php'; ?>
+<?php include './partials/alert.php'; ?>
 
 <?php
 $_SESSION['page_permission'] = 'teacher';
@@ -12,7 +12,7 @@ include './includes/session.inc.php';
 include './includes/permission.inc.php';
 ?>
 
-<main class="container-fluid w-90 mt-4 p-0 bg-white <?= $view ?>">
+<main class="container-fluid w-90 mt-4 mb-5 p-0 bg-white <?= $view ?>">
 <?php include 'includes/student.inc.php'; ?>
 </main>
 <?php require $_SERVER['DOCUMENT_ROOT'].'/sabanges/partials/footer.php'; ?>
