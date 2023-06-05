ba81279d2c2075d01d7fc4693a1c13d07ed26c71
Justine <ujusine@yahoo.com>
2023-06-05 10:50:39 +0800

    update

--- promotion.php
+++ promotion.php
@@ -13,6 +13,50 @@ include './includes/session.inc.php';
 include './includes/permission.inc.php';
 ?>
 
+<?php if (isset($_GET['id']) && isset($_GET['lrn'])) { ?>
+<div class="modal fade" id="promotionResult" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="promotionResultLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="promotionResultLabel">Promoted/Retained students</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Successfully promoted students: </p>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>
+                #
+              </th>
+              <th>
+                Lrn
+              </th>
+              <th>
+                Remark
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php for ($i=0; $i < count($_GET['id']); $i++) { ?>
+            <tr>
+              <td><?= $i + 1 ?></td>
+              <td><?= $_GET['lrn'][$i]; ?></td>
+              <td><?= $_GET['remark'][$i]; ?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php } ?>
+
 <main class="container-fluid w-90 mt-4 mb-5 ">
   <?php include './partials/nav_records_tabs.php'; ?>
   <div class="p-2 border border-top-0">
@@ -20,9 +64,11 @@ include './includes/permission.inc.php';
         <div>
         <?php include './partials/nav_filter_student.php'; ?>
         </div>
+        <div class="load-promotion">
         <?php
         require $_SERVER['DOCUMENT_ROOT'].'/sabanges/includes/promotion_retention.inc.php';
         ?>
+        </div>
     </form>
 </main>
 
