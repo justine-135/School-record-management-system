02af0b6f0d09f4e5a4b5c5192178bffdd738d66d
Justine <ujusine@yahoo.com>
2023-06-05 10:50:29 +0800

    update

--- grading.php
+++ grading.php
@@ -14,19 +14,6 @@ include './includes/permission.inc.php';
 ?>
 
 <main class="container-fluid w-90 mt-4 mb-5 ">
-  
-<!-- <div class="toast-container position-absolute toast-grading">
-  <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-header">
-      <strong class="me-auto">Notification</strong>
-      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-    <div class="toast-body">
-      <strong>GRADING PERIOD IS CLOSED!</strong>
-    </div>
-  </div>
-</div> -->
-
   <?php include './partials/nav_records_tabs.php'; ?>
   <div class="p-2 border border-top-0">
     <div>
